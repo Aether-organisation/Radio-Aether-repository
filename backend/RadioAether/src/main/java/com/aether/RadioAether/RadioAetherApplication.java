a014ffd8faa4e26e6e7eea6944fe32e0a8ca2918
PRORIX <romengilberto08@gmail.com>
2026-02-09 15:52:45 +0000

    Created basic structure #3  and Spring project #4

---
+++ backend/RadioAether/src/main/java/com/aether/RadioAether/RadioAetherApplication.java
@@ -0,0 +1,13 @@
+package com.aether.RadioAether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioAetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioAetherApplication.class, args);
+	}
+
+}
---
+++ backend/RadioAether/src/test/java/com/aether/RadioAether/RadioAetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aether.RadioAether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioAetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
