7cca23aaf8d42fb538d4cd9fbfb632d887566135
PRORIX <romengilberto08@gmail.com>
2026-02-12 18:15:28 +0000

    Created nearest station endpoint in back #18

--- backend/RadioAether/src/main/java/com/aether/RadioAether/controller/RadioController.java
+++ backend/RadioAether/src/main/java/com/aether/RadioAether/controller/RadioController.java
@@ -1,11 +1,13 @@
 package com.aether.RadioAether.controller;
 
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aether.RadioAether.model.dto.request.LocationRequest;
 import com.aether.RadioAether.model.dto.response.StationDTO;
 import com.aether.RadioAether.service.RadioService;
 
@@ -27,6 +29,13 @@ public class RadioController {
     @GetMapping("/mock")
     public ResponseEntity<StationDTO> getMockRadio() {
         return ResponseEntity.ok(radioService.getMockRadio());
+    }
+
+    @PostMapping("/nearest")
+    public ResponseEntity<StationDTO> getNearestStation(@RequestBody LocationRequest request) {
+        StationDTO recommendedStation = radioService.findNearestStation(request);
+        
+        return ResponseEntity.ok(recommendedStation);
     }
 }
 
